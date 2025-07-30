cc5aa770e1e4cf4e5bbf8fa69a35b81ca48c4c83
Nino <nino.parviainen@gmail.com>
2025-07-30 15:08:01 +0300

    same

--- scripts/mazeSpawner.gd
+++ scripts/mazeSpawner.gd
@@ -1,12 +1,17 @@
 extends Node
 
+#var firstMaze = preload("res://maze1")
+#var secondMaze = preload("res://maze2")
+#var thirdMaze = preload("res://maze3")
+#var fourthMaze = preload("res://maze4")
 
-var randomNumber = RandomNumberGenerator.new()
+#var randomNumber = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print_debug(randomNumber)
+	#print_debug(randomNumber)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
